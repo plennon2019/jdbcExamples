99717b0567d70857e66070e6002b4f903022433c
plennon <plennon@gmit.ie>
2025-03-05 14:27:14 +0000

    Added simple logger to write errors to a file. Updated pom to include the logger lib

--- src/main/java/ie/atu/jdbc/pool/DatabaseUtils.java
+++ src/main/java/ie/atu/jdbc/pool/DatabaseUtils.java
@@ -12,7 +12,7 @@ public class DatabaseUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseUtils.class);
     private static final HikariDataSource dataSource;
     private static final String URL = "jdbc:mysql://localhost:3306/exampledatabase";
-    private static final String USERNAME = "root";
+    private static final String USERNAME = "rot";
     private static final String PASSWORD = "password";
 
     //notice the static has no name?
